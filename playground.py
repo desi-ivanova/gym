090234e8827a294f2971f84a953139e49dc33b63
desi-ivanova <desi.r.ivanova@gmail.com>
2025-11-28 11:59:43 +0000

    playground args

--- playground.py
+++ playground.py
@@ -18,14 +18,14 @@ NUM_NODES = 4
 ### This is a minimal configuration for training a nanogpt model with a given strategy.
 ### The strategy can be swapped out for custom logic by writing a new strategy class.
 
+
 class IndexSelector:
     def __init__(self, p):
         self.state = {}
         self.p = p
 
     @abstractmethod
-    def get_indices(self, param, iteration):
-        ...
+    def get_indices(self, param, iteration): ...
 
 
 class RandomIndexSelector(IndexSelector):
@@ -33,6 +33,7 @@ class RandomIndexSelector(IndexSelector):
         return torch.bernoulli(
             torch.full(param.shape, self.p, device=param.device)
         ).bool()
+
 
 class SPARTAStrategy(Strategy):
     def __init__(
@@ -45,36 +46,49 @@ class SPARTAStrategy(Strategy):
         index_selector = RandomIndexSelector(p_sparta)
 
         super().__init__(**kwargs)
-        
-        self.optim_spec = optim_spec if isinstance(optim_spec, OptimSpec) else OptimSpec.from_str(optim_spec)
+
+        self.optim_spec = (
+            optim_spec
+            if isinstance(optim_spec, OptimSpec)
+            else OptimSpec.from_str(optim_spec)
+        )
         self.index_selector = index_selector
 
-    def step(self, ):
+    def step(
+        self,
+    ):
         with torch.no_grad():
             for param in self.model.parameters():
                 if not param.requires_grad or param.grad is None:
                     continue
 
-                indices_mask = self.index_selector.get_indices(
-                    param, self.local_step
-                )
+                indices_mask = self.index_selector.get_indices(param, self.local_step)
 
                 broadcast(indices_mask, src=0)
                 sparse_data = param.data[indices_mask]
-                
+
                 all_reduce(sparse_data, op=torch.distributed.ReduceOp.SUM)
                 sparse_data /= self.num_nodes
 
                 param.masked_scatter_(indices_mask, sparse_data)
-    
+
         self.optim.step()
         super().step()
 
+
 def main():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--dataset", type=str, default="owt")
+    arg_parser.add_argument(
+        "--dataset", type=str, default="shakespeare"
+    )  # owt or shakespeare
+    # add gpt2_small v gpt_sbase as argument
+    arg_parser.add_argument(
+        "--model", type=str, default="gpt2_small"
+    )  # gpt2_small or gpt2_sbase
     args = arg_parser.parse_args()
     dataset = args.dataset
+    model_name = args.model
+    print(f"Using dataset: {dataset}, model: {model_name}")
 
     # Get datasets - this will take a while the first time, as the dataset has to be imported and processed.
     train_dataset, vocab_size = get_dataset(
@@ -85,16 +99,18 @@ def main():
         end_pc=0.005 * NUM_NODES if dataset == "owt" else 0.99,
     )
     val_dataset, vocab_size = get_dataset(
-        dataset, 
-        block_size=1024, 
-        device="cpu",
-        start_pc=0.99, 
-        end_pc=1.0
+        dataset, block_size=1024, device="cpu", start_pc=0.99, end_pc=1.0
     )
 
     device = get_device()
 
-    gpt_config = GPTConfig.gpt2_sbase()
+    if model_name == "gpt2_small":
+        gpt_config = GPTConfig.gpt2_small()
+    elif model_name == "gpt2_sbase":
+        gpt_config = GPTConfig.gpt2_sbase()
+    else:
+        raise NotImplementedError
+
     gpt_config.vocab_size = vocab_size
     model = GPT(gpt_config)
 
@@ -137,12 +153,12 @@ def main():
         num_nodes=NUM_NODES,
         device=device,
         batch_size=16,
-        minibatch_size=8, # Gradient accumulation to ensure we can fit in memory. Make this even lower for smaller devices. 
+        minibatch_size=8,  # Gradient accumulation to ensure we can fit in memory. Make this even lower for smaller devices.
         shuffle=False,
         val_size=256,
         val_interval=100,
-        wandb_project='sparta',
-        run_name=f'sparta-run'
+        wandb_project="sparta",
+        run_name=f"sparta-run",
     )
 
 
